666a3ff11fea6b3db8bb23e285f515b4e1dd97b5
Max <mverost4422@gmail.com>
2018-12-03 22:09:09 -0500

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,7 +1,14 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = ["cardOne", "cardTwo"];
+var cardOne = cards[0];
+console.log("user flipped queen");
+var cardTwo = cards[2];
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!")
+  } else {
+    alert("Sorry, try again")
+  }
+}
