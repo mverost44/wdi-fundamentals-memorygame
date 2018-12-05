afb6e94a19c54df2581b19fd2f1b62cb32418a91
Max <mverost4422@gmail.com>
2018-12-05 12:24:04 -0500

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,21 +1,49 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+  }
+];
+
+
 var cardsInPlay = [];
+cardsInPlay.push('cards[cardId].rank');
 
 var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-console.log("You found a match!");
+alert("You found a match!");
 } else {
-console.log("Sorry, try again.");
+alert("Sorry, try again.");
 }
 }
 
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  if (cardsInPlay.length === 2) {
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   checkForMatch();
+  //checkForMatch() might have to go in cardsInPlay.length === 2, but the length is only 1??
+  if (cardsInPlay.length === 2) {
+
   }
 };
-cardsInPlay.push('cards[cardId]');
+
 
 flipCard(0);
 flipCard(2);
